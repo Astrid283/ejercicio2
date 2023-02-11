4cc2d143df6db9da4901242056f50da43d1ccec6
Astrid283 <124418683+Astrid283@users.noreply.github.com>
2023-02-11 02:15:17 -0600

    Tarea 2
    Realice tarea 2 de calcular promedio de estudiante

---
+++ PromedioFinalEstudiante/PromedioFinalEstudiante/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace PromedioFinalEstudiante
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Nota1 = new System.Windows.Forms.Label();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.Nota2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.Nota3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNota4 = new System.Windows.Forms.TextBox();
+            this.Nota4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtObservacion = new System.Windows.Forms.TextBox();
+            this.Observacion = new System.Windows.Forms.Label();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.Promedio = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 0;
+            // 
+            // Nota1
+            // 
+            this.Nota1.AutoSize = true;
+            this.Nota1.Location = new System.Drawing.Point(55, 63);
+            this.Nota1.Name = "Nota1";
+            this.Nota1.Size = new System.Drawing.Size(43, 16);
+            this.Nota1.TabIndex = 1;
+            this.Nota1.Text = "Nota1";
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(159, 57);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(100, 22);
+            this.txtNota1.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(159, 175);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(159, 89);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(100, 22);
+            this.txtNota2.TabIndex = 9;
+            // 
+            // Nota2
+            // 
+            this.Nota2.AutoSize = true;
+            this.Nota2.Location = new System.Drawing.Point(55, 89);
+            this.Nota2.Name = "Nota2";
+            this.Nota2.Size = new System.Drawing.Size(43, 16);
+            this.Nota2.TabIndex = 8;
+            this.Nota2.Text = "Nota2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(279, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 7;
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(159, 119);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(100, 22);
+            this.txtNota3.TabIndex = 12;
+            // 
+            // Nota3
+            // 
+            this.Nota3.AutoSize = true;
+            this.Nota3.Location = new System.Drawing.Point(55, 119);
+            this.Nota3.Name = "Nota3";
+            this.Nota3.Size = new System.Drawing.Size(43, 16);
+            this.Nota3.TabIndex = 11;
+            this.Nota3.Text = "Nota3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(287, 212);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 16);
+            this.label6.TabIndex = 10;
+            // 
+            // txtNota4
+            // 
+            this.txtNota4.Location = new System.Drawing.Point(159, 147);
+            this.txtNota4.Name = "txtNota4";
+            this.txtNota4.Size = new System.Drawing.Size(100, 22);
+            this.txtNota4.TabIndex = 15;
+            // 
+            // Nota4
+            // 
+            this.Nota4.AutoSize = true;
+            this.Nota4.Location = new System.Drawing.Point(55, 147);
+            this.Nota4.Name = "Nota4";
+            this.Nota4.Size = new System.Drawing.Size(43, 16);
+            this.Nota4.TabIndex = 14;
+            this.Nota4.Text = "Nota4";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(295, 220);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 16);
+            this.label8.TabIndex = 13;
+            // 
+            // txtObservacion
+            // 
+            this.txtObservacion.Location = new System.Drawing.Point(159, 240);
+            this.txtObservacion.Name = "txtObservacion";
+            this.txtObservacion.Size = new System.Drawing.Size(100, 22);
+            this.txtObservacion.TabIndex = 19;
+            // 
+            // Observacion
+            // 
+            this.Observacion.AutoSize = true;
+            this.Observacion.Location = new System.Drawing.Point(55, 240);
+            this.Observacion.Name = "Observacion";
+            this.Observacion.Size = new System.Drawing.Size(84, 16);
+            this.Observacion.TabIndex = 18;
+            this.Observacion.Text = "Observacion";
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(159, 206);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(100, 22);
+            this.txtPromedio.TabIndex = 17;
+            // 
+            // Promedio
+            // 
+            this.Promedio.AutoSize = true;
+            this.Promedio.Location = new System.Drawing.Point(55, 212);
+            this.Promedio.Name = "Promedio";
+            this.Promedio.Size = new System.Drawing.Size(66, 16);
+            this.Promedio.TabIndex = 16;
+            this.Promedio.Text = "Promedio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtObservacion);
+            this.Controls.Add(this.Observacion);
+            this.Controls.Add(this.txtPromedio);
+            this.Controls.Add(this.Promedio);
+            this.Controls.Add(this.txtNota4);
+            this.Controls.Add(this.Nota4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.Nota3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.Nota2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.Nota1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Nota1;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.Label Nota2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.Label Nota3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNota4;
+        private System.Windows.Forms.Label Nota4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtObservacion;
+        private System.Windows.Forms.Label Observacion;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.Label Promedio;
+    }
+}
+
---
+++ PromedioFinalEstudiante/PromedioFinalEstudiante/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PromedioFinalEstudiante
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double Nota1, Nota2, Nota3, Nota4, Promedio;
+            string Observacion;
+
+            Nota1 = double.Parse(txtNota1.Text);
+            Nota2 = double.Parse(txtNota2.Text);
+            Nota3 = double.Parse(txtNota3.Text);
+            Nota4 = double.Parse(txtNota4.Text);
+            Promedio = (Nota1 + Nota2 + Nota3 + Nota4) / 4;
+
+            if (Promedio<=64)
+            {
+                Observacion = "Reprobado";
+                txtPromedio.Text = Promedio.ToString("N2");
+                txtObservacion.Text = Observacion;
+            }else if (Promedio >= 65 && Promedio <= 100)
+            {
+                Observacion = "Aprobado";
+                txtPromedio.Text = Promedio.ToString("N2");
+                txtObservacion.Text = Observacion;
+
+            }
+        }
+    }
+}
